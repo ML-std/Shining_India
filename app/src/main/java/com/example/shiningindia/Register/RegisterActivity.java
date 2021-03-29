0e48d406ac2e8b0876fc7444b5d534e569f16fa4
ML-std <ML-std@github. com>
2021-03-29 19:06:38 +0300

    Initial commit

---
+++ app/src/main/java/com/example/shiningindia/Register/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.example.shiningindia.Register;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.shiningindia.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
